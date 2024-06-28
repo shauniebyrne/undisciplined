f8bb884f99bfe5e6122c1c02a20d522dcf511168
shauniebyrne <byr21002@byui.edu>
2024-06-27 17:50:08 -0700

    moving on to button

---
+++ js/buttonclick.js
@@ -0,0 +1,7 @@
+let buttonClick = document.getElementById("contactBtn");
+
+
+// Create Function to show box when button is clicked
+function createBox() {
+    buttonClick.innerHTML = "Hello World!"
+}
